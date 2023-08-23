562d0c630df432ef2bc21eb6c728e5701c13e54b
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-08-23 14:37:28 +0530

    Update validate_email_address.py

--- validate_email_address.py
+++ validate_email_address.py
@@ -1,17 +1,18 @@
 import re
 
-user_input = input("Do you want to enter an email address? (Yes/No) :").lower()
+while True:
+    user_input = input("Do you want to enter an email address? (Yes/No) :").lower()
 
-if user_input == 'no':
-    file_read = open("email_txt.txt","r")
-    print(file_read.read())
+    if user_input == 'no':
+        file_read = open("email_txt.txt","r")
+        print(file_read.read())
+        break
 
-elif user_input == 'yes':
-    email = input("Enter the email address :").lower()
-    pattern = r"\w+(|.)\w+@[a-z]+\.[A-Za-z]{2,3}"
-    out = re.search(pattern,email)
-    if out:
-        file_write = open(r"email_txt.txt","a")
-        file_write.write(out.group()+"\n")
-        file_write.close()
-
+    elif user_input == 'yes':
+        email = input("Enter the email address :").lower()
+        pattern = r"\w+(|.)\w+@[a-z]+\.[A-Za-z]{2,3}"
+        out = re.search(pattern,email)
+        if out:
+            file_write = open(r"email_txt.txt","a")
+            file_write.write(out.group()+"\n")
+            file_write.close()
