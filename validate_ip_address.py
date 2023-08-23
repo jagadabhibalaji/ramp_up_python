8fb5aad5f8d16146785576eb818a2f9209619b06
jagadabhibalaji <141623034+jagadabhibalaji@users.noreply.github.com>
2023-08-23 14:38:13 +0530

    Update validate_ip_address.py

--- validate_ip_address.py
+++ validate_ip_address.py
@@ -1,31 +1,33 @@
 import re
 from ping3 import ping
 
-user_input = input("Do you want to enter an ip address? (Yes/No) :").lower()
+while True:
+    user_input = input("Do you want to enter an ip address? (Yes/No) :").lower()
 
-if user_input == 'no':
-    file_read = open("ip_address.txt", "r")
-    print(file_read.read())
+    if user_input == 'no':
+        file_read = open("ip_address.txt", "r")
+        print(file_read.read())
+        break
 
-if user_input == 'yes':
-    # it should be contain four octes
-    input_ip_address = input("Enter the ip address, each octet should be within 0-255 :")
-    pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
-    ip_address = re.match(pattern, input_ip_address)
+    if user_input == 'yes':
+        # it should be contain four octes
+        input_ip_address = input("Enter the ip address, each octet should be within 0-255 :")
+        pattern = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+        ip_address = re.match(pattern, input_ip_address)
 
 
-    if ip_address:
-        octets = input_ip_address.split(".")
-        ip = [i for i in octets if int(i) <= 255]
-        if len(ip) == 4:
-            valid_ip = ip_address.group()
-            response_time = ping(valid_ip)
+        if ip_address:
+            octets = input_ip_address.split(".")
+            ip = [i for i in octets if int(i) <= 255]
+            if len(ip) == 4:
+                valid_ip = ip_address.group()
+                response_time = ping(valid_ip)
 
-            with open("ip_address.txt", "a") as fw:
-                if response_time is not None:
-                    fw.write(f"validate_ip: {valid_ip} (Response time: {response_time} ms)\n")
-                else:
-                    fw.write(f"invalidate_ip: {valid_ip} (Unreachable)\n")
-    else:
-        with open("ip_address.txt", "a") as fw:
-            fw.write(f"invalid_ip_format: {input_ip_address}\n")
+                with open("ip_address.txt", "a") as file_write:
+                    if response_time is not None:
+                        file_write.write(f"validate_ip: {valid_ip} (Response time: {response_time} ms)\n")
+                    else:
+                        file_write.write(f"invalidate_ip: {valid_ip} (Unreachable)\n")
+        else:
+            with open("ip_address.txt", "a") as file_write:
+                file_write.write(f"invalid_ip_format: {input_ip_address}\n")
